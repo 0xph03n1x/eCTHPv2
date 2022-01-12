fae9a31dcdfdf3e8403811170f3ddca1b8e284c5
AutismPrime <lazarovmihail97@gmail.com>
2022-01-12 15:58:25 +0200

    12.01.2022

--- Powershell hunting/Hunting Web Shells/Get-ChildItem.ps1
+++ Powershell hunting/Hunting Web Shells/Get-ChildItem.ps1
@@ -1,1 +1,1 @@
-get-childitem -recurse -include "*.php" | select-string "(mail|fsockopen|pfsockopen|exec\b|system\b|passthru|eval\b|base64_decode)" | % {"$($_.filename):$($_.line)"} | out-gridview
+get-childitem -recurse -include "*.php" | select-string "(mail|fsockopen|pfsockopen|exec\b|system\b|passthru|eval\b|base64_decode)" | ForEach-Object {"$($_.filename):$($_.line)"} | out-gridview
